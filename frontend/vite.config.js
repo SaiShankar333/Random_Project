a07376e337504fa6f136fb41a86ebfc2e6e85fd7
SaiShankar333 <saishankar333@outlook.com>
2025-10-29 18:26:41 +0530

    Initial commit for Render deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Random_Project/', // 👈 important for GitHub Pages
   server: {
     port: 5173,
     proxy: {
@@ -13,4 +15,3 @@ export default defineConfig({
     }
   }
 })
-
